39ff0869b05a9e336986739b63856f2f037c6b9f
Diego Fernando Cuesta <diego.cuesta@farmatodo.com>
2019-03-28 22:51:23 -0500

    Initial Commit

---
+++ Picap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Picap
+//
+//  Created by Medios Digitales on 3/28/19.
+//  Copyright © 2019 Diego Fernando Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
